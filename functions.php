294398a68b35bfcb9c4ded098b752c80685b502b
Umar Farouq Mohammed <upharouk@gmail.com>
2018-11-28 18:21:27 +0000

    Add featured post functionality to theme functions

--- functions.php
+++ functions.php
@@ -47,3 +47,52 @@ function li_a_class ( $class ) {
 	return $class;
 }
 add_filter ( 'nav_menu_link_attributes', 'li_a_class' );
+
+# Function to select a featured post
+function sm_featured_box () {
+  add_meta_box (
+    'sm_meta', __( 'Feature Post', 'sm-textdomain' ), 'sm_featured_callback', 'post'
+  );
+}
+
+# Function to show feature checkbox
+function sm_featured_callback ( $post ) {
+  $featured = get_post_meta ( $post->ID ); ?>
+
+  <p>
+    <div class="sm-row-content">
+      <label for="meta-checkbox">
+        <input type="checkbox" name="meta-checkbox" value="yes" id="meta-checkbox"
+        <?php if ( isset( $featured['meta-checkbox'] ) ) checked ( $featured['meta-checkbox'][0], 'yes' ); ?>>
+        <?php _e( 'Feature this post' ); ?>
+      </label>
+    </div>
+  </p>
+
+  <?php
+}
+
+add_action ( 'add_meta_boxes', 'sm_featured_box' );
+
+# Function to save meta checkbox
+function sm_meta_save ( $post_id ) {
+  # Check save status
+  $is_autosave = wp_is_post_autosave ( $post_id );
+  $is_revision = wp_is_post_revision ( $post_id );
+  $is_valid_nonce = ( isset( $_POST['sm_nonce'] )
+    && wp_verify_nonce ( $_POST['sm_nonce'], basename( __FILE__ ) ) ) ? 'true' : 'false';
+
+  # Exit script on post type status
+  if ( $is_autosave || $is_revision || !$is_valid_nonce ) {
+    return;
+  }
+
+  # Check for input and saves
+  if ( isset( $_POST['meta-checkbox'] ) ) {
+    update_post_meta ( $post_id, 'meta-checkbox', 'yes' );
+  } else {
+    update_post_meta ( $post_id, 'meta-checkbox', '' );
+  }
+}
+
+add_action ( 'save_post', 'sm_meta_save' );
