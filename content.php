23aba7f32ed79388e38f3e70f4e8add6fa40a88b
Umar Farouq Mohammed <upharouk@gmail.com>
2018-11-28 16:37:43 +0000

    Add content part template to theme

--- content.php
+++ content.php
@@ -5,8 +5,8 @@
       <div class="post-image" style="background-image: url(<?php the_post_thumbnail_url(); ?>); background-position: center; background-size: cover; background-repeat: no-repeat; background-origin: initial"></div>
     <?php endif ?>
     <h5 class="mt-0"><a href="<?php the_permalink(); ?>" class="text-body black-hans"><?php the_title(); ?></a></h5>
-    <p><span class="fa fa-calendar-o"></span> \<?php echo get_the_date(); ?></p>
+    <p><span class="fa fa-calendar-o"></span> <?php echo get_the_date(); ?></p>
     <p class="text-secondary"><?php the_excerpt(); ?></p>
-    <a href="post.html"><button class="btn btn-outline-secondary">Read post</button></a>
+    <a href="<?php the_permalink(); ?>"><button class="btn btn-outline-secondary">Read post</button></a>
   </div>
 </div>
